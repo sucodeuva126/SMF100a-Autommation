e1a6544a886f5635053fbda0a92aee9305fd6e40
sucodeuva126 <gus.games126@gmail.com>
2026-06-11 11:16:54 -0300

    first commands

--- SMF100a_commands.py
+++ SMF100a_commands.py
@@ -1,2 +1,7 @@
 import pyvisa
+smfIP = "10.10.10.10"
 rm = pyvisa.ResourceManager()
+rm.list_resources()
+smf = rm.open_resource('TCPIP::'+smfIP+'::inst0::INSTR')
+print(smf.query('*IDN?'))
+smf.close()
